f939c7e5cba77f7ed9a3848ef04c3e8a3421ca36
sumitha <sundaresansumitha@gmail.com>
2022-02-16 09:47:10 -0800

    gettop main page steps

--- features/steps/Gettop_main_page.py
+++ features/steps/Gettop_main_page.py
@@ -16,6 +16,14 @@ def verify_number_of_items(context,expected_count):
 def click_watch(context):
     context.app.header.click_watch()
 
+@when('Hover over MAC')
+def hover_over_mac(context):
+    context.app.header.hover_over_MAC()
 
+@then('Verify {expected_count} items are shown under MAC category')
+def verify_all_mac_items(context,expected_count):
+    context.app.header.verify_all_mac_items(expected_count)
 
-
+@when('Hover over MAC and click on each products and verify correct product_page opens')
+def hover_and_click_mac_product(context):
+    context.app.header.hover_and_click_mac_product()
