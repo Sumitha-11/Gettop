19b63ce60cd51f92f8ce32e904f4406625844954
sumitha <sundaresansumitha@gmail.com>
2022-02-18 12:33:25 -0800

    Added steps to watch product page

--- features/steps/watch_product_page.py
+++ features/steps/watch_product_page.py
@@ -4,6 +4,21 @@ from behave import given, when, then
 def click_product(context):
     context.app.product_page.click_and_verify_product()
 
+@then('Click on watch series 5 product')
+def click_watch_series(context):
+    context.app.product_page.click_product()
+
+@then('Add the product to cart')
+def click_add_to_cart(context):
+    context.app.product_page.click_add_to_cart()
+
+@then('Click the Checkout button')
+def click_checkout(context):
+    context.app.product_page.click_checkout()
+
+@when('Click on cart icon')
+def click_cart_icon(context):
+    context.app.product_page.click_cart_icon()
 
 
 
