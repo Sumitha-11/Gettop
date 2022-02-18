1614490336c62c4269cea6ba2497e9ac0f4f6ccc
sumitha <sundaresansumitha@gmail.com>
2022-02-18 12:30:38 -0800

    Application

--- app/application.py
+++ app/application.py
@@ -1,4 +1,19 @@
+from pages.cart_page import CartPage
+from pages.checkout_page import CheckoutPage
+from pages.footer_page import FooterPage
+from pages.header import Header
+from pages.main_page import MainPage
+from pages.product_page import ProductPage
+
+
 class Application:
 
     def __init__(self,driver):
         self.driver = driver
+
+        self.cart_page     = CartPage(self.driver)
+        self.checkout_page = CheckoutPage(self.driver)
+        self.header        = Header(self.driver)
+        self.footer_page   = FooterPage(self.driver)
+        self.main_page     = MainPage(self.driver)
+        self.product_page  = ProductPage(self.driver)
