93b5875a185bf369a9a9238f3c17f42d1c622442
sumitha <sundaresansumitha@gmail.com>
2022-02-22 09:51:07 -0800

    Added steps for account login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,3 +3,6 @@ from pages.base_page import Page
 class MainPage(Page):
     def open_gettop(self):
         self.open_page()
+
+    def open_login_page(self):
+        self.open_page(f'my-account/')
